571d28bbf550cb26bc694fd450037425937d1f84
slurmulon <anon@blaqkmail.com>
2015-01-30 12:32:10 -0800

    type check for rest hooks

--- src/sloth/hooks.go
+++ src/sloth/hooks.go
@@ -29,7 +29,7 @@ func (hook *RestHook) Kill() {
 
 // Hook resource
 
-// var _ RestfulHookResource = (*RestHookResource)(nil)
+var _ RestfulHookResource = (*RestHookResource)(nil)
 
 type RestfulHookResource interface {
   RestfulResource
