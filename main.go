4d979886fcde948706ff5c5b1220961f29e8376a
slurmulon <anon@blaqkmail.com>
2015-01-29 20:11:07 -0800

    updated vars

--- main.go
+++ main.go
@@ -8,15 +8,15 @@ import (
 
 // https://cloud.google.com/appengine/docs/go/gettingstarted/helloworld
 
-type FooResource struct {
+type Foo struct {
   sloth.RestResource
 }
 
-func (FooResource) Get(values url.Values) (int, interface{}) {
+func (Foo) Get(values url.Values) (int, interface{}) {
   return 200, "hello world"
 }
 
-func (FooResource) Post(values url.Values) (int, interface{}) {
+func (Foo) Post(values url.Values) (int, interface{}) {
   data := map[string]string{"yum": "thanks"}
   return 200, data
 }
@@ -24,12 +24,11 @@ func (FooResource) Post(values url.Values) (int, interface{}) {
 func main() {
   fmt.Println("Sloth example")
 
-  // slothResource := &FooResource{ &sloth.RestResource{ UrlSlug: "/hello" } }
+  slothResource := Foo{ sloth.RestResource{ UrlSlug: "/hello" } }
+  slothService  := sloth.RestService{ BaseUrl: "http://foo.bar/api" }
 
-  var api = sloth.RestService{ BaseUrl: "http://foo.bar/api" }
-
-  api.AddResource(&FooResource{ sloth.RestResource{ UrlSlug: "/hello" } })
-  api.Start(3000)
+  slothService.AddResource(&slothResource)
+  slothService.Start(3000)
 
   // fmt.Println("sloth resource " + slothResource)
 }
